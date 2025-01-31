05bceb59413e63192fc992d32c63c7297ce000cc
Lhekdup <lhekduptenzin@gmail.com>
2025-01-31 16:09:49 -0500

    added lab week 1

---
+++ Week01/lab_week1.py
@@ -0,0 +1,12 @@
+# Sample Coding Questions 01 Week 01
+# Lhekdup Tenzin
+num = [1,4,7,9]
+a = 1
+b = 2
+c = 3
+d = 4
+e = a - ((b ** c) // d) + (a % c)
+temperature = 32.6
+print("The temperature today is:", '{0:.3f}'.format(temperature))
+userAge = input("How old are you? ")
+print("Now showing the shop items filtered by age:", userAge)
