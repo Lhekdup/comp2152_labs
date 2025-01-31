ab206d485acd2fc296fb06e9768455a9bdb71414
Lhekdup <lhekduptenzin@gmail.com>
2025-01-31 11:21:50 -0500

    add q4

--- Week04/lab_week4.py
+++ Week04/lab_week4.py
@@ -81,6 +81,9 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+# Lab4 Q4
+belt = []
+
 # LAB04 - Q1
 # Define the Monster Power
 monster_powers = {
@@ -165,7 +168,8 @@ power_roll = random.choice(["Fire Magic", "Freezing time", "Super Hearing"])
 
 #L4 - Q3 - Increase the Monster Strength
 m_combat_powers = min(6, m_combat_strength + monster_powers[power_roll])
-print("The Monster Combat Strength is: " + str(m_combat_strength) + "Using the " + power_roll + "Magic Power!")
+print("The Monster Combat Strength is: " + str(m_combat_strength) + " Using the " + power_roll + "Magic Power!")
+
 # Loop while the monster and the player are alive. Call fight sequence functions
 print("You meet the monster. FIGHT!!")
 while m_health_points > 0 and health_points > 0:
