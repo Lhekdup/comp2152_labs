8fd9dbd062d10112f34a14f734ba201c20293248
Lhekdup <lhekduptenzin@gmail.com>
2025-02-09 14:37:52 -0500

    Finished with Assignment1

--- assignment1/assignment1_101491383.py
+++ assignment1/assignment1_101491383.py
@@ -1,40 +1,89 @@
-'''
+#a
+"""
 Author: <Lhekdup Tenzin>
 Assignment: #1
-'''
+"""
 
+#b
 gym_member = "Alex Alliton"         #string
 preferred_weight_kg = 20.5          #float
 highest_reps = 25                   #int
 membership_active = True            #boolean
 
+#c
 workout_stats = {
-    "Alex": (30, 45, 20),
+    "Alex": (30, 45, 50),
     "Jamie": (29, 39, 10),
-    "Taylor": (40, 35, 30)
+    "Taylor": (60, 44, 30)
 }
 print(workout_stats)                #dict
 
+#d
+total_workout_stat = {}
 for name, minute in workout_stats.items():
-    workout_stats[name+"_Total"] = workout_stats.pop(name)
-    workout_stats.update({name + "_Total": sum(minute)})
+    total_workout_stat.update({name + "_Total": sum(minute)})
 
-print(workout_stats)                #dict
+print(total_workout_stat)           #dict
 
+#e
 names = list(workout_stats.keys())
-minutes = list(workout_stats.values())
 workout_list = []
 for i in range(0, len(names)):
-    exercises = []
-    exercises.append(names[i])
-    for j in range(0, len(minutes)):
-        if (j == 0):
+    exercises = [names[i]]
+    for j in range(0, len(names)):
+        if j == 0:
             exercises.append("yoga")
-        elif (j == 1):
+        elif j == 1:
             exercises.append("running")
         else:
             exercises.append("weightlifting")
     workout_list.append(exercises)
-print(workout_list)                     # list
+print(workout_list)                   #list
+
+#f
+yoga_running = []
+for name, minute in workout_stats.items():
+    yoga_running.append({name : minute[:2]})
+print(yoga_running)
+
+last_two = []
+for name, minute in workout_stats.items():
+    last_two.append({name: minute})
+last_two.pop(0)
+print(last_two)
+
+#g
+for name, minute in workout_stats.items():
+    if sum(minute) >= 120:
+        print("Great Job staying active " + name + "!")
+
+#h
+username = input("Enter a friend's name: ")
+if username in workout_stats:
+    print(workout_stats[username])
+    for name, minute in workout_stats.items():
+        workout_stats.update({name: sum(minute)})
+    print(workout_stats[username])
+else:
+    print("Friend " + username + " not found in the record!.")
+
+#i
+highest_total_minutes = max(total_workout_stat, key=total_workout_stat.get)
+lowest_total_minutes = min(total_workout_stat, key=total_workout_stat.get)
+h = highest_total_minutes[:-6]
+l = lowest_total_minutes[:-6]
+print(h, "has the highest total workout minutes and" ,l, "has the lowest total workout minutes.")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
